60ff8d86d874fb4b9835fa9ba7f3e4e822583f0c
iman01 <iman.esfandiyar71@gmail.com>
2022-10-30 09:07:04 +0100

    Single frame printer added

--- src/can_bridge.cpp
+++ src/can_bridge.cpp
@@ -1,10 +1,13 @@
 #include <memory>
+#include <iostream>
+#include <iterator>
+#include <set>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "can_msgs/msg/frame.hpp"
 
-
+using namespace std;
 using std::placeholders::_1;
 
 class CanBridge : public rclcpp::Node
@@ -18,11 +21,46 @@ class CanBridge : public rclcpp::Node
     }
 
   private:
-    void can_callback(const can_msgs::msg::Frame & can_msg) const
+
+    
+
+    void showset(set<int> g)
     {
-      RCLCPP_INFO(this->get_logger(), "I heard id: %u", can_msg.id);
+      set<int>::iterator it;
+      for (it = g.begin(); it != g.end(); ++it)
+          cout << '\t' << *it;
+      cout << '\n';
     }
+
+    void make_set(const can_msgs::msg::Frame & can_msg)
+    {
+      can_id_set.emplace(can_msg.id);
+      showset(can_id_set);
+    }
+
+    void single_can_msg_printer(const can_msgs::msg::Frame & can_msg) const
+    {
+      cout << "id: " << can_msg.id << endl << "data:"<< endl;
+
+      for (int each_byte : can_msg.data)
+      {
+        cout << "- " << each_byte << endl;
+      }
+    
+    }
+
+    void can_callback(const can_msgs::msg::Frame & can_msg) 
+    {
+      if(can_msg.id == 200)
+        single_can_msg_printer(can_msg);
+      // make_set(can_msg)
+      // RCLCPP_INFO(this->get_logger(), "I heard id: %u", can_msg.id);
+      
+    }
+    
     rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr can_subscription_;
+    set<int> can_id_set;
+
 };
 
 int main(int argc, char * argv[])
