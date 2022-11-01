b8a1265c4576a2a7d9a4a3ef82c7ad9ed9933c4b
iman01 <iman.esfandiyar71@gmail.com>
2022-11-01 13:16:57 +0100

    all publishers added

--- src/agrorob_interface.cpp
+++ src/agrorob_interface.cpp
@@ -1,98 +1,179 @@
 #include <memory>
-#include <iostream>
-#include <iterator>
-#include <set>
+#include <math.h>
+
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "can_msgs/msg/frame.hpp"
 
-#include "agrorob_interfaces/mode_manager.hpp"
+// #include "agrorob_interfaces/mode_manager.hpp"
 
 #include "agrorob_msgs/msg/engine_state.hpp"
-// #include "agrorob_can_msgs/msg/failure_state.hpp"
-// #include "agrorob_can_msgs/msg/mode_control.hpp"
-// #include "agrorob_can_msgs/msg/robot_control.hpp"
-// #include "agrorob_can_msgs/msg/robot_state.hpp"
-// #include "agrorob_can_msgs/msg/sprayer_control.hpp"
-// #include "agrorob_can_msgs/msg/tool_control.hpp"
-// #include "agrorob_can_msgs/msg/tool_state.hpp"
+#include "agrorob_msgs/msg/failure_state.hpp"
+#include "agrorob_msgs/msg/tool_state.hpp"
+#include "agrorob_msgs/msg/remote_state.hpp"
+#include "agrorob_msgs/msg/robot_state.hpp"
+
+// #include "agrorob_msgs/msg/mode_control.hpp"
+// #include "agrorob_msgs/msg/robot_control.hpp"
+
+// #include "agrorob_msgs/msg/sprayer_control.hpp"
+// #include "agrorob_msgs/msg/tool_control.hpp"
+
 
 using namespace std;
 using std::placeholders::_1;
-using namespace agrorob_interface;
+
 
 
 
 class AgrorobInterface : public rclcpp::Node
 {
   public:
-    AgrorobInterface() : Node("agrorob_interface"), modeManager()
+    AgrorobInterface() : Node("agrorob_interface")//, modeManager()
     {
-      
-
       raw_can_sub_ = this->create_subscription<can_msgs::msg::Frame>("from_can_bus", 10, std::bind(&AgrorobInterface::can_callback, this, _1));
+      
+      remote_state_pub_ = this->create_publisher<agrorob_msgs::msg::RemoteState>("/agrorob/remote_state", 10);
+      robot_state_pub_ = this->create_publisher<agrorob_msgs::msg::RobotState>("/agrorob/robot_state", 10);
+      tool_stats_pub_ = this->create_publisher<agrorob_msgs::msg::ToolState>("/agrorob/tool_state", 10);
       engine_stats_pub_ = this->create_publisher<agrorob_msgs::msg::EngineState>("/agrorob/engine_state", 10);
-
-
+      failure_state_pub_ = this->create_publisher<agrorob_msgs::msg::FailureState>("/agrorob/failure_state", 10);
     }
 
   private:
     
+    // auto failure_state_message = agro::FailureState();
+    // std::shared_ptr<agrorob_msgs::msg::FailureState> failure_state_message = agrorob_msgs::msg::FailureState();
+    // auto engine_state_message = agrorob_msgs::msg::EngineState();
     
-
-    void showset(set<int> g)
-    {
-      set<int>::iterator it;
-      for (it = g.begin(); it != g.end(); ++it)
-          cout << '\t' << *it;
-      cout << '\n';
-    }
-
-    void make_set(const can_msgs::msg::Frame & can_msg)
-    {
-      can_id_set.emplace(can_msg.id);
-      showset(can_id_set);
-    }
-
-    void single_can_msg_printer(const can_msgs::msg::Frame & can_msg) const
-    {
-      cout << "id: " << can_msg.id << endl << "data:"<< endl;
-
-      for (int each_byte : can_msg.data)
-      {
-        cout << "- " << each_byte << endl;
-      }
-    
-    }
 
     void can_callback(const can_msgs::msg::Frame & can_msg) 
     {
-      auto can_msg_ = can_msg;
-      // if(can_msg.id == 200)
-      //   single_can_msg_printer(can_msg);
-      // make_set(can_msg)
-      // RCLCPP_INFO(this->get_logger(), "I heard id: %u", can_msg.id);
       
       
-
       switch(can_msg.id)
       {
+
         case 100: // This id belongs to msgs from manual control pad 
         {
-          modeManager.update_remote_state(can_msg_);
-          // modeManager.is_agrorob_ready();
+
+          if(remote_state_msg.auto_mode_enabled != can_msg.data[2])
+          {
+            if(can_msg.data[2] == 0) RCLCPP_INFO(this->get_logger(), "REMOTE: Operation mode: --MANUAL--");
+            else RCLCPP_INFO(this->get_logger(), "REMOTE: Operation mode: --AUTO--");
+          }
+
+          if(remote_state_msg.stop_engine != can_msg.data[1])
+          {
+            if(can_msg.data[1] == 0) RCLCPP_INFO(this->get_logger(), "REMOTE: Engine: --ACTIVE--");
+            else RCLCPP_INFO(this->get_logger(), "REMOTE: Engine: --SHUTDOWN--");
+          }
+
+          if(remote_state_msg.stop_drivers != can_msg.data[0])
+          {
+            if(can_msg.data[0] == 0) RCLCPP_INFO(this->get_logger(), "REMOTE: Drivers: --ACTIVE--");
+            else RCLCPP_INFO(this->get_logger(), "REMOTE: Drivers: --SHUTDOWN--");
+          }
+
+          remote_state_msg.stop_drivers       =  can_msg.data[0]; //0 - inactive; 1 - STOP of all drives active
+          remote_state_msg.stop_engine        =  can_msg.data[1]; //0 - the internal combustion engine can work; 1 - engine shutdown
+          remote_state_msg.auto_mode_enabled  =  can_msg.data[2]; //0 - manual mode; 1 - automatic mode
+          auto_mode_enabled                   =  can_msg.data[2];
+          
+
+
+          // auto_mode_enabled = modeManager.update_remote_state(can_msg_, failure_state_message);
+          
+          // if(!auto_mode_enabled)
+          //   RCLCPP_INFO(this->get_logger(), agrorob_not_ready);
+
           break;
+        }
+
+
+        case 31:
+        {
+          robot_state_msg.left_front_wheel_encoder_imp  = (can_msg.data[1] << 8) + can_msg.data[0]; // high << 8 + low
+          robot_state_msg.right_front_wheel_encoder_imp = (can_msg.data[3] << 8) + can_msg.data[2];
+          robot_state_msg.left_rear_wheel_encoder_imp   = (can_msg.data[5] << 8) + can_msg.data[4];
+          robot_state_msg.right_rear_wheel_encoder_imp  = (can_msg.data[7] << 8) + can_msg.data[6];
+
+          robot_state_pub_->publish(robot_state_msg);
+          break;
+        }
+
+        case 32: 
+        {
+          robot_state_msg.left_front_wheel_turn_angle_rad  = (can_msg.data[0] - 90) * (M_PI/180) ; //
+          robot_state_msg.right_front_wheel_turn_angle_rad = (can_msg.data[1] - 90) * (M_PI/180) ;
+          robot_state_msg.left_rear_wheel_turn_angle_rad   = (can_msg.data[2] - 90) * (M_PI/180) ;
+          robot_state_msg.right_rear_wheel_turn_angle_rad  = (can_msg.data[3] - 90) * (M_PI/180) ;
+
+          robot_state_msg.left_front_wheel_rotational_speed_rpm   = can_msg.data[4];
+          robot_state_msg.right_front_wheel_rotational_speed_rpm  = can_msg.data[5];
+          robot_state_msg.left_rear_wheel_rotational_speed_rpm    = can_msg.data[6];
+          robot_state_msg.right_rear_wheel_rotational_speed_rpm   = can_msg.data[7];
+          robot_state_pub_->publish(robot_state_msg);
+          break;
+        }
+
+        case 33: 
+        {
+
+          tool_state_msg.vertical_actuator_pos             = can_msg.data[0];
+          tool_state_msg.horizental_actuator_pos           = can_msg.data[1];
+          tool_state_msg.tiller_left_limit_enabled         = can_msg.data[2];
+          tool_state_msg.tiller_right_limit_enabled        = can_msg.data[3];
+          tool_state_msg.sowing_shaft_rotational_speed_rpm = can_msg.data[4];
+          tool_state_msg.fertilizer_tank1_level            = can_msg.data[5];
+          tool_state_msg.fertilizer_tank2_level            = can_msg.data[6];
+          tool_stats_pub_->publish(tool_state_msg);
+          break;
+        }
+
+        case 34: 
+        {
+          if(can_msg.data[0] <= 60)  // degrees minus 60(e.g. 65 means 65-60 = 5 degrees)
+            tool_state_msg.tool_level_rad = can_msg.data[0] * (M_PI/180);
+          else
+            tool_state_msg.tool_level_rad = (can_msg.data[0] - 60) * (M_PI/180);
+
+          tool_state_msg.hydraulic_oil_tem_celsius           = can_msg.data[2]; 
+          tool_state_msg.hydraulic_oil_pressure              = can_msg.data[3]; 
+          tool_state_msg.hydraulic_oil_under_min_level       = can_msg.data[4]; 
+          tool_state_msg.hydraulic_oil_above_max_level       = can_msg.data[5]; 
+          tool_state_msg.wheels_average_rotational_speed_rpm = can_msg.data[6];
+          tool_stats_pub_->publish(tool_state_msg);
+          break; 
         }
         
         case 51: 
         {
-          auto engine_state_message = agrorob_msgs::msg::EngineState();
-          engine_state_message.engine_coolant_temp_celsius = can_msg.data[3];
-          engine_stats_pub_->publish(engine_state_message);
-          RCLCPP_INFO(this->get_logger(), "I heard id: %u", can_msg.id);
+          engine_state_msg.engine_rotation_speed_rpm  = (can_msg.data[1] << 8) + can_msg.data[0]; // high << 8 + low
+          engine_state_msg.engine_running               = can_msg.data[2];                        // 0 - off; 1 - the engine is running
+          engine_state_msg.engine_coolant_temp_celsius  = can_msg.data[3];                        // 0 - 100 - value determining the temperature ( o C )
+          engine_state_msg.engine_fuel_level_percent    = can_msg.data[5];                        // 0-100 - value specifying% of tank filling
+          engine_state_msg.engine_oil_pressure_bar      = can_msg.data[7];                        // 0 - 100 (100 means 10 bar)
+          engine_stats_pub_->publish(engine_state_msg);
           break;
-          
+        }
+
+        case 99: 
+        {
+          failure_state_msg.left_front_turn_failed           = can_msg.data[0];
+          failure_state_msg.right_front_turn_failed          = can_msg.data[1];
+          failure_state_msg.left_rear_turn_failed            = can_msg.data[2];
+          failure_state_msg.right_rear_turn_failed           = can_msg.data[3];
+          failure_state_msg.tool_vertical_actuator_failed    = can_msg.data[4];
+          failure_state_msg.tool_horizontal_actuator_failed  = can_msg.data[5];
+          failure_state_msg.tool_level_sensor_failed         = can_msg.data[6];
+
+          for(auto any_failure_end : can_msg.data)
+            if(any_failure_end) RCLCPP_INFO(this->get_logger(), "Sensor failure detected!");
+
+          failure_state_pub_->publish(failure_state_msg);
+          break;
         }
         
         
@@ -102,18 +183,30 @@ class AgrorobInterface : public rclcpp::Node
     }
     
     rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr raw_can_sub_;
+
+    rclcpp::Publisher<agrorob_msgs::msg::RemoteState>::SharedPtr remote_state_pub_;
+    rclcpp::Publisher<agrorob_msgs::msg::RobotState>::SharedPtr robot_state_pub_;
+    rclcpp::Publisher<agrorob_msgs::msg::ToolState>::SharedPtr tool_stats_pub_;
     rclcpp::Publisher<agrorob_msgs::msg::EngineState>::SharedPtr engine_stats_pub_; 
+    rclcpp::Publisher<agrorob_msgs::msg::FailureState>::SharedPtr failure_state_pub_; 
+
+    agrorob_msgs::msg::RemoteState remote_state_msg = agrorob_msgs::msg::RemoteState();  
+    agrorob_msgs::msg::RobotState robot_state_msg = agrorob_msgs::msg::RobotState();
+    agrorob_msgs::msg::ToolState tool_state_msg = agrorob_msgs::msg::ToolState();
+    agrorob_msgs::msg::EngineState engine_state_msg = agrorob_msgs::msg::EngineState();
+    agrorob_msgs::msg::FailureState failure_state_msg =  agrorob_msgs::msg::FailureState();
     
-    // auto failure_state_message = agrorob_msgs::msg::FailureState();
+    
       // auto mode_control_message = agrorob_msgs::msg::ModeControl();
       // auto robot_control_message = agrorob_msgs::msg::RobotControl();
-      // auto robot_state_message = agrorob_msgs::msg::RobotState();
+     
       // auto sprayer_control_message = agrorob_msgs::msg::SprayerControl();
       // auto tool_control_message = agrorob_msgs::msg::ToolControl();
-      // auto tool_state_message = agrorob_msgs::msg::ToolState();
+      
     
-    ModeManager modeManager;
-    set<int> can_id_set;
+    
+    bool auto_mode_enabled;
+    
     
 
 };
